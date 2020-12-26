54743041c020aa44cd05c6d42ec34d5d5efdaebb
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-12-26 10:14:48 +0900

    상품 주문

---
+++ src/main/java/jpabook/jpastore/controller/OrderController.java
@@ -0,0 +1,46 @@
+package jpabook.jpastore.controller;
+
+import jpabook.jpastore.domain.Member;
+import jpabook.jpastore.domain.item.Item;
+import jpabook.jpastore.service.ItemService;
+import jpabook.jpastore.service.MemberService;
+import jpabook.jpastore.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    private final MemberService memberService;
+
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
